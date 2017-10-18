8f274c2970baa114f90086e964f1509635570069
Ionel Gog <gogionel@gmail.com>
2017-10-18 20:44:29 +0100

    Added YCSB run scripts.

---
+++ scripts/run_naiad_ycsb_5_machines.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# $1 = loadtarget
+# $2 = timeslice
+# $3 = numelements
+parallel-ssh -v -h ~/caelum_5_machines -t 0 -p 50 -i 'PROCID=`./falkirk-experiments/scripts/get_falkirk_proc_id.sh` ; cd falkirk/Naiad/ ; mono-sgen ./FaultToleranceExamples/bin/Release/FaultToleranceExamples.exe ycsbdd -ycsbconfigfile /home/srguser/streaming-benchmarks-yahoo/conf/localConf.yaml -minimallog -t 10 -n 5 -p $PROCID -h @/home/${USER}/machines_5 -loadtarget $1 -timeslice $2 -numelements $3 &> ycsb-caelum-$PROCID.log'
