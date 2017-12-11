6dfab7ef6484926842cdf84520bec442ba898173
Ionel Gog <gogionel@gmail.com>
2017-12-11 09:53:56 +0000

    Add selective run scripts.

---
+++ scripts/run_naiad_ycsb_selective_delay_5_epochs_duplicate_25_machines.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+parallel-ssh -v -h ~/caelum_25_machines -t 0 -p 26 -i 'PROCID=`./falkirk-experiments/scripts/get_falkirk_proc_id.sh` ; cd falkirk/Naiad/ ; export MONO_GC_PARAMS=nursery-size=256m ; ./FaultToleranceExamples/bin/Release/FaultToleranceExamples.exe ycsb-selective -ycsbconfigfile /home/srguser/streaming-benchmarks-yahoo/conf/localConf.yaml -minimallog -t 4 -n 25 -p $PROCID -h @/home/${USER}/machines_25 --reachability 10 --deadlocktimeout 5 -loadtarget 120000000 -timeslice 1000 -numelements 12000000000 -delayeachepoch 5 &> ycsb-caelum-$PROCID.log'
