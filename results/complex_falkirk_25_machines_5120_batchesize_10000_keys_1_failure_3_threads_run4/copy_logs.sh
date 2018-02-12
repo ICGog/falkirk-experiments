734b6048c4729472cfbbde78be66ef3d88b6f276
Ionel Gog <gogionel@gmail.com>
2018-02-12 01:53:11 +0000

    Added results.

---
+++ results/complex_falkirk_25_machines_5120_batchesize_10000_keys_1_failure_3_threads_run4/copy_logs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scp srguser@caelum-301:/tmp/falkirk/ftmanager.log ./
+scp srguser@caelum-409:/tmp/falkirk/fastPipe.021.log ./
+scp srguser@caelum-409:/tmp/falkirk/inputLatency.021.log ./
+scp srguser@caelum-301:/tmp/falkirk/controller.000.log ./
